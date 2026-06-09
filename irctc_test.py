fbec3228ca5f0db0504fbfb158bdfc26842155f1
Arunchegz <109329119+Arunchegz@users.noreply.github.com>
2026-06-09 21:04:13 +0530

    Update irctc_test.py

--- irctc_test.py
+++ irctc_test.py
@@ -3,18 +3,28 @@ from playwright.sync_api import sync_playwright
 with sync_playwright() as p:
 
     browser = p.chromium.launch(
-        headless=True,
+        headless=False,
         args=[
-            "--disable-http2"
+            "--disable-blink-features=AutomationControlled"
         ]
     )
 
-    page = browser.new_page()
+    page = browser.new_page(
+        user_agent=(
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+            "AppleWebKit/537.36 "
+            "(KHTML, like Gecko) "
+            "Chrome/137.0.0.0 Safari/537.36"
+        )
+    )
 
     page.goto(
-        "https://www.google.com",
-        timeout=60000
+        "https://www.irctc.co.in",
+        wait_until="domcontentloaded",
+        timeout=120000
     )
+
+    page.screenshot(path="irctc.png")
 
     print(page.title())
 
