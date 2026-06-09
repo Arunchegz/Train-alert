8171682cf9ac338cb18100894b6e538a799192bd
Arunchegz <109329119+Arunchegz@users.noreply.github.com>
2026-06-09 14:06:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,51 @@
+from fastapi import FastAPI
+from fastapi import Form
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi import Request
 
+from models import *
+from sqlalchemy import insert
+
+app = FastAPI()
+
+templates = Jinja2Templates(
+    directory="templates"
+)
+
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+
+    return templates.TemplateResponse(
+        "index.html",
+        {"request": request}
+    )
+
+@app.post("/add-alert")
+def add_alert(
+    train_number: str = Form(...),
+    from_station: str = Form(...),
+    to_station: str = Form(...),
+    journey_date: str = Form(...),
+    class_code: str = Form(...),
+    telegram_chat_id: str = Form(...)
+):
+
+    conn = engine.connect()
+
+    conn.execute(
+        insert(alerts).values(
+            train_number=train_number,
+            from_station=from_station,
+            to_station=to_station,
+            journey_date=journey_date,
+            class_code=class_code,
+            telegram_chat_id=telegram_chat_id
+        )
+    )
+
+    conn.commit()
+
+    return {
+        "success": True
+    }
