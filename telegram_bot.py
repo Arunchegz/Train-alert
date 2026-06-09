c42d53620f1c736f88c7efaf3821de21b28fa8fe
Arunchegz <109329119+Arunchegz@users.noreply.github.com>
2026-06-09 15:42:23 +0530

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,1 +1,14 @@
+import requests
 
+BOT_TOKEN = "7669839735:AAFw6DOheN69uuTDuQ13BqFnf9YDJvnXBM0"
+
+def send_alert(chat_id, message):
+
+    requests.post(
+        f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
+        json={
+            "chat_id": chat_id,
+            "text": message
+        },
+        timeout=30
+    )
