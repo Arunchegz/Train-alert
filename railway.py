2738692eebf3822fe36c472414f13161ce774a44
Arunchegz <109329119+Arunchegz@users.noreply.github.com>
2026-06-09 14:59:17 +0530

    Update railway.py

--- railway.py
+++ railway.py
@@ -2,6 +2,7 @@ import requests
 
 URL = "https://trainticketapi.railyatri.in/api/trains-between-station-with-sa.json"
 
+
 def get_status(
     train_number,
     from_station,
@@ -9,6 +10,7 @@ def get_status(
     journey_date,
     class_code
 ):
+
     params = {
         "from": from_station,
         "to": to_station,
@@ -17,19 +19,22 @@ def get_status(
         "device_type_id": "6"
     }
 
-    r = requests.get(URL, params=params, timeout=30)
+    r = requests.get(
+        URL,
+        params=params,
+        timeout=30
+    )
 
     data = r.json()
 
-    for train in data.get(
-        "train_between_stations",
-        []
-    ):
+    for train in data.get("train_between_stations", []):
+
         if train["train_number"] != train_number:
             continue
 
-        for cls in train["sa_data"]:
-            if cls["booking_class"] == class_code:
-                return cls["availibility"]
+        for seat in train.get("sa_data", []):
+
+            if seat["booking_class"] == class_code:
+                return seat["availibility"]
 
     return "NOT FOUND"
