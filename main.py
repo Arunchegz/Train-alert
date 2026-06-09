4cbd4fcf7b327afac046d088f934d335790a2bf2
Arunchegz <109329119+Arunchegz@users.noreply.github.com>
2026-06-09 15:53:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,12 @@ from fastapi import FastAPI, Form, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
+from sqlalchemy import insert, select, update
+
 from models import engine, alerts
-from sqlalchemy import insert
+from railway import get_status
+from telegram_bot import send_alert
+from scheduler import scheduler
 
 app = FastAPI()
 
@@ -12,6 +16,7 @@ templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
 def home(request: Request):
+
     return templates.TemplateResponse(
         request=request,
         name="index.html"
@@ -48,3 +53,81 @@ def add_alert(
         "success": True,
         "message": "Alert saved"
     }
+
+
+def check_alerts():
+
+    print("Checking alerts...")
+
+    conn = engine.connect()
+
+    rows = conn.execute(
+        select(alerts)
+    ).fetchall()
+
+    for row in rows:
+
+        if row.notified:
+            continue
+
+        try:
+
+            status = get_status(
+                row.train_number,
+                row.from_station,
+                row.to_station,
+                row.journey_date,
+                row.class_code
+            )
+
+            print(
+                f"Train={row.train_number} "
+                f"Class={row.class_code} "
+                f"Status={status}"
+            )
+
+            if (
+                status
+                and isinstance(status, str)
+                and status.startswith("AVAILABLE")
+            ):
+
+                send_alert(
+                    row.telegram_chat_id,
+                    f"""🚆 Seat Available!
+
+Train: {row.train_number}
+Route: {row.from_station} → {row.to_station}
+
+Class: {row.class_code}
+
+Status: {status}
+"""
+                )
+
+                conn.execute(
+                    update(alerts)
+                    .where(alerts.c.id == row.id)
+                    .values(notified=True)
+                )
+
+                conn.commit()
+
+                print(
+                    f"Notification sent for alert {row.id}"
+                )
+
+        except Exception as e:
+
+            print(
+                f"Error checking alert {row.id}: {e}"
+            )
+
+
+scheduler.add_job(
+    check_alerts,
+    "interval",
+    minutes=1
+)
+
+scheduler.start()
